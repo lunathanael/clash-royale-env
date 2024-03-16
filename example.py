4d45f761c35ad2dfbed57fe692808c2828f22097
Nathanael Lu <lunathanael@gmail.com>
2024-03-16 02:49:38 -0400

    added ffmpeg for imageio, testing for multigames

--- example.py
+++ example.py
@@ -7,30 +7,6 @@ def random_action():
     card = randint(0, 3)
     return x, y, card
 
-interface = Interface() # Start the game interface
-print("Interface Initialized.")
-
-interface.start_classic_deck_battle() # Initiate game
-print("Game Requested.")
-
-while not interface.in_game(): # Wait for game to be in play-ready state
-    continue
-print("Game Started!")
-
-imgs = [] # array to store images
-sum = 0 # random sum to randomize action timings
-while not interface.is_game_over(): # render frames until game over
-    imgs.append(interface.get_image())
-    sum += randint(1, 2)
-    if sum >= 200 - len(imgs) / 10:
-        sum = 0
-        x, y, card = random_action()
-        interface.play_card(x, y, card)
-
-print("Game outcome:", interface.determine_victor())
-interface.exit_game() # Exit the game-over screen
-print(f"Game had a total of {len(imgs)} frames rendered.")
-
 import imageio
 import numpy as np
 def rgb_arrays_to_mp4(rgb_arrays, output_path, fps: int = 30):
@@ -49,5 +25,30 @@ def rgb_arrays_to_mp4(rgb_arrays, output_path, fps: int = 30):
     # Close the writer to finalize the video file
     writer.close()
 
-rgb_arrays_to_mp4(imgs, "out.mp4")
-print("Game video stored at out.mp4")
+interface = Interface() # Start the game interface
+print("Interface Initialized.")
+
+for i in range(100):
+    interface.start_classic_deck_battle() # Initiate game
+    print("Game Requested.")
+
+    while not interface.in_game(): # Wait for game to be in play-ready state
+        continue
+    print("Game Started!")
+
+    imgs = [] # array to store images
+    sum = 0 # random sum to randomize action timings
+    while not interface.is_game_over(): # render frames until game over
+        imgs.append(interface.get_image())
+        sum += randint(1, 2)
+        if sum >= 200 - len(imgs) / 10:
+            sum = 0
+            x, y, card = random_action()
+            interface.play_card(x, y, card)
+
+    print("Game outcome:", interface.determine_victor())
+    interface.exit_game() # Exit the game-over screen
+    print(f"Game had a total of {len(imgs)} frames rendered.")
+
+    rgb_arrays_to_mp4(imgs, f"./replays/game_{i}.mp4")
+    print(f"Game video stored at replays/game_{i}.mp4")
